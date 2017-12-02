63a9f83fcff495d5217af795ef81487eb5853911
Rune Botten <rune.botten@sparebank1.no>
2017-12-02 18:39:27 +0100

    Proper order of assert_equal params

--- 1.rb
+++ 1.rb
@@ -1,3 +1,5 @@
+# https://adventofcode.com/2017/day/1
+
 require "minitest/autorun"
 
 def captcha(number, step = 1)
@@ -10,17 +12,17 @@ end
 
 class DayOneTests < Minitest::Test
     def test_part1
-        assert_equal(captcha(1122), 3)
-        assert_equal(captcha(1111), 4)
-        assert_equal(captcha(1234), 0)
-        assert_equal(captcha(91212129), 9)
+        assert_equal(3, captcha(1122))
+        assert_equal(4, captcha(1111))
+        assert_equal(0, captcha(1234))
+        assert_equal(9, captcha(91212129))
     end
     
     def test_part2
-        assert_equal(captcha(1212, "1212".chars.count / 2), 6)
-        assert_equal(captcha(1221, "1221".chars.count / 2), 0)
-        assert_equal(captcha(123425, "123425".chars.count / 2), 4)
-        assert_equal(captcha(123123, "123123".chars.count / 2), 12)
-        assert_equal(captcha(12131415, "12131415".chars.count / 2), 4)
+        assert_equal(6, captcha(1212, "1212".chars.count / 2))
+        assert_equal(0, captcha(1221, "1221".chars.count / 2))
+        assert_equal(4, captcha(123425, "123425".chars.count / 2))
+        assert_equal(12, captcha(123123, "123123".chars.count / 2))
+        assert_equal(4, captcha(12131415, "12131415".chars.count / 2))
     end
 end
